ac561a14e02d49d51a90d09d6b8f53984eddf110
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-11-01 23:18:27 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -42,12 +42,6 @@ git clone https://github.com/wilversings/maxwell /usr/share/plasma/plasmoids/max
 # apply the le custom weenOS plymouth boot theme
 sudo plymouth-set-default-theme logo-slider
 
-# and then forcefully rebuild initrd!!!!... maybe this one will work
-QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel")"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
-
 # download the news
 
 curl -L -o /usr/weenos/news/thenews \
