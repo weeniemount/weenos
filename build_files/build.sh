3419ee108f0372a150c6c54c452bbffe2ff2f0d1
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-11-02 10:57:07 +0100

    sddm fix?

--- build_files/build.sh
+++ build_files/build.sh
@@ -47,3 +47,12 @@ sudo plymouth-set-default-theme logo-slider
 curl -L -o /usr/weenos/news/thenews \
   https://github.com/weeniemount/thenews-linux/releases/latest/download/thenews
 chmod +x /usr/weenos/news/thenews
+
+# SDDM fix from the bazzite repo as of Fedora 43
+sed -i '/^StartLimitBurst=2$/a # Wait for driver loading to be complete so that graphics work\n# (https://github.com/sddm/sddm/issues/1917)\nWants=systemd-udev-settle.service\nAfter=systemd-udev-settle.service' /usr/lib/systemd/system/sddm.service
+
+# and then forcefully rebuild initrd!!!!... maybe this one will work
+QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel")"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
